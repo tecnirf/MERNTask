f2196ad3a5e8e04fdf66fbaa9a52c7f88bf9d2b9
Edi Ferraro <tecnirf@MacBook-Pro-de-Edi.local>
2020-08-27 20:00:22 -0500

    ajustes para deployment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // Puerto de la app
-const port  '0.0.0.0',= process.env.port  '0.0.0.0',|| 4000;
+const port = process.env.port || 4000;
 
 // Importar rutas
 
